a32f4c97b7eedbe49302f50ee919795ba03d0096
Ayrtonalvaro <ayrtonalvaro1973@gmail.com>
2022-11-13 12:15:21 -0300

    Implementation add producto to list

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,42 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
-import Modal from './components/Modal'
-import Form from './components/Form'
 import List from './components/List'
 
-const INITIAL_STATE = [
-  {
-    nameProduct: 'Milk',
-  },
-  {
-    nameProduct: 'Coca Cola',
-  },
-]
+
 
 interface Product {
   nameProduct: string
 }
 
 function App() {
-  const [item, setItem] = useState<Array<Product>>([]);
-  const [modal, setModal] = useState <boolean>(false);
-  const [amountProducts, setAmountProducts] = useState<number>(0);
-
-  
+  const [item, setItem] = useState<Array<Product>>([])
+  const [modal, setModal] = useState<boolean>(false)
+  const [newProduct, setNewProduct] = useState(" ")
 
   const toggleModal = () => {
     setModal(!modal)
   }
 
-  useEffect(() => {
-    setItem(INITIAL_STATE), []
-  })
 
-  const [viewModal, setViewModal] = useState<boolean>(false)
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("agregar producto")
+    const productAdd = {
+      nameProduct: newProduct
+    }
+    console.log(productAdd)
+    setItem(item.concat(productAdd))
+    setNewProduct("")
+    setModal(!modal)
+    console.log(item)
+  }
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewProduct(event.target.value)
+    console.log(newProduct)
+  }
+
+  
 
   return (
     <div className="App">
@@ -44,8 +48,7 @@ function App() {
             </h1>
           </header>
           <h4 className="text-xl font-semibold flex justify-center">
-            {' '}
-            0 item(s)
+            {item.length} item(s)
           </h4>
 
           <List products={item} />
@@ -64,25 +67,27 @@ function App() {
               <h2 className="flex justify-center font-barlow font-black text-2xl">
                 Add item
               </h2>
-              <form>
+              <form onSubmit={handleSubmit}>
                 <input
                   className="h-10 mt-5 border-2 border-sky-500 w-[310px]"
-                  name="product"
                   type="text"
-                  placeholder="product"
+                  value={newProduct}
+                  onChange={handleChange}
                 ></input>
+                <div className="flex justify-between gap-3">
+                  <button
+                    className="mt-5 border rounded h-10 w-40 bg-slate-100  text-black"
+                    onClick={toggleModal}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="mt-5 border rounded w-40 bg-sky-500 text-white"
+                  >
+                    Add
+                  </button>
+                </div>
               </form>
-              <div className="flex justify-between gap-3">
-                <button
-                  className="mt-5 border rounded h-10 w-40 bg-slate-100  text-black"
-                  onClick={toggleModal}
-                >
-                  Close
-                </button>
-                <button className="mt-5 border rounded w-40 bg-sky-500 text-white">
-                  Add
-                </button>
-              </div>
             </div>
           </>
         )}
